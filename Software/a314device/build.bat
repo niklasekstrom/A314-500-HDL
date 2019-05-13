8afbfdee31846f18bd968d06052652e3224b697d
Niklas Ekström <mail@niklasekstrom.nu>
2019-05-13 15:32:35 +0200

    Updated files to Quartus Prime Lite 18.1.

--- Software/a314device/build.bat
+++
@@ -1,1 +0,0 @@
-vc romtag.asm a314driver.c vblank_server.asm -nostdlib -o a314.device
--- Software/a314fs/comment_out_sections.py
+++
@@ -1,5 +0,0 @@
-with open('a314fs.asm', 'r+b') as f:
-    text = f.read()
-    text = text.replace('section', ';section')
-    f.seek(0, 0)
-    f.write(text)
